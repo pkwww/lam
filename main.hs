c9842c1c3e6b001df33ff9027aabd7e32c48d857
pkwww <pkwww.2000@gmail.com>
2018-10-28 03:58:39 +0000

    no type for now; alphaconversion

--- main.hs
+++ main.hs
@@ -1,21 +1,35 @@
--- data LamType = INT | BOOL | Fun LamType LamType
-data LamVar = MkVar String
-data LamTerm = LamV LamVar | LamI Int | LamB Bool | LamLam LamVar LamTerm | LamApp LamTerm LamTerm
 
-instance Show LamVar where
-  show (MkVar s) = s
+
+-- don't want to bother with Church numerals
+data Nat = Zero | Succ Nat
+data LamTerm where 
+  Id :: String -> LamTerm
+  LamLam :: String -> LamTerm -> LamTerm 
+  LamApp :: LamTerm -> LamTerm -> LamTerm
+  NatLit :: Nat -> LamTerm
+ 
+natToInt :: Nat -> Integer
+natToInt Zero = 0
+natToInt (Succ n) = 1 + natToInt n
+
+instance Show Nat where
+  show n = show (natToInt n)
 
 instance Show LamTerm where
-  show (LamV var) = show var
-  show (LamI x) = show x
-  show (LamB b) = show b
+  show (Id s) = show s
   show (LamLam var term) = "\\" ++ show var ++ "." ++ show term
   show (LamApp term1 term2) = "(" ++ show term1 ++ ")(" ++ show term2 ++ ")"
+  show (NatLit n) = show n
 
-varX = MkVar "x"
-termX = LamV varX
-one = LamI 1
-lamIdentity = LamLam varX termX
-app = LamApp lamIdentity one
+alphaConversion :: String -> String -> LamTerm -> LamTerm
+alphaConversion to from (Id s) = if s == from then Id to else Id s
+alphaConversion to from (LamLam var term) = if var == from 
+                                            then LamLam to (alphaConversion to from term) 
+                                            else LamLam var (alphaConversion to from term)
+alphaConversion to from (LamApp term1 term2) = LamApp (alphaConversion to from term1) (alphaConversion to from term2)
+alphaConversion to from n = n
 
-main = print app
+--substitude :: LamTerm -> String -> LamTerm -> LamTerm
+
+
+main = print ""

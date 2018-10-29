e0dacd8cda90f25cf611f4abc608439aee15931d
pkwww <pkwww.2000@gmail.com>
2018-10-29 16:02:55 +0000

    alpha conversion become undefined when free var capturing occur. can i make it type level error? comment out substitude for now, seems son't need it for beta reduction

--- main.hs
+++ main.hs
@@ -8,9 +8,9 @@ data LamTerm where
   LamApp :: LamTerm -> LamTerm -> LamTerm
   NatLit :: Nat -> LamTerm
 
-data Context a where
-  Nil :: Context a
-  Cons :: a -> Context a -> Context a
+type Context a = [a]
+type VarContext = Context String
+type Variable = String
 
 natToInt :: Nat -> Integer
 natToInt Zero = 0
@@ -25,25 +25,34 @@ instance Show LamTerm where
   show (LamApp term1 term2) = "(" ++ show term1 ++ ")(" ++ show term2 ++ ")"
   show (NatLit n) = show n
 
-alphaConversion :: String -> String -> LamTerm -> LamTerm
-alphaConversion to from (Id s) = if s == from then Id to else Id s
-alphaConversion to from (LamLam var term) = if var == from 
-                                            then LamLam to (alphaConversion to from term) 
-                                            else LamLam var (alphaConversion to from term)
-alphaConversion to from (LamApp term1 term2) = LamApp (alphaConversion to from term1) (alphaConversion to from term2)
-alphaConversion _ _ n = n
+alphaConversion :: VarContext -> Variable -> Variable -> LamTerm -> LamTerm
+alphaConversion context to from (Id s) = 
+  if s == from 
+  then if to `inContext` context
+        then undefined
+        else Id to 
+  else Id s
+alphaConversion context to from (LamLam var term) = 
+  let extendContext = var : context in
+    if var == from
+    then LamLam to (alphaConversion extendContext to from term)
+    else LamLam var (alphaConversion extendContext to from term)
+alphaConversion context to from (LamApp term1 term2) = 
+  LamApp (alphaConversion context to from term1) (alphaConversion context to from term2)
+alphaConversion context _ _ n = n
 
--- not yet consider context, free var should not captured by bounded var, need a variable context
-substitude :: LamTerm -> String -> LamTerm -> LamTerm
+inContext :: Variable -> VarContext -> Bool
+inContext = elem
+
+{- substitude :: LamTerm -> String -> LamTerm -> LamTerm
 substitude (Id s) var substitudeTerm = if s == var then substitudeTerm else (Id s)
 substitude (LamLam boundedVar term) substitudeVar substitudeTerm = if substitudeVar == boundedVar 
   then (LamLam boundedVar term) 
   else (LamLam boundedVar (substitude term substitudeVar substitudeTerm))
-substitude (LamApp term1 term2) var substitudeTerm = LamApp (substitude term1 var substitudeTerm) (substitude term2 var substitudeTerm)
-substitude n _ _ = n
+substitude (LamApp term1 term2) var substitudeTerm = 
+  LamApp (substitude term1 var substitudeTerm) (substitude term2 var substitudeTerm)
+substitude n _ _ = n -}
 
-substitudeTerm1 = Id "x"
 termTest = LamLam "x" (Id "y")
-termAfter = substitude termTest "y" substitudeTerm1
 
-main = print termAfter
+main = print termTest
